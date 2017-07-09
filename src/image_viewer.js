24c30ad8e4955b53d455acecc50719f0a94efb0c
everchenko <everchenko@at-consulting.ru>
2017-07-09 13:16:44 +0300

    add split

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,11 +1,18 @@
-import big from '../assets/big.jpg';
+// import big from '../assets/big.jpg';
 import small from '../assets/small.jpg';
 import '../styles/image_viewer.css';
 
-let image = document.createElement('img');
-image.src = small;
-document.body.appendChild(image);
 
+export default () => {
+	let image = document.createElement('img');
+	image.src = small;
+	document.body.appendChild(image);
+};
+
+
+
+/*
 let bigImage = document.createElement('img');
 bigImage.src = big;
 document.body.appendChild(bigImage);
+*/
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+/*
 import sum from './sum';
 import './image_viewer';
 
 window.sum = sum;
 const total = sum(10, 5); 
 console.log(total);
+*/
+
+const button = document.createElement('button');
+button.innerText = 'Ckuck me';
+button.onclick = () => {
+	System.import('./image_viewer').then(module => {
+		console.log(module);
+		module.default();
+	});
+};
+
+document.body.appendChild(button);
+
